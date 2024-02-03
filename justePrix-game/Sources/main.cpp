3dc1d64f356b6a3fa81f22e9808d70e6e7a04de3
David M <103978628+Zatiji@users.noreply.github.com>
2024-02-02 23:19:52 -0500

    ajout des fichiers déja crée

--- justePrix-game/Sources/main.cpp
+++ justePrix-game/Sources/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <random>
 
 //fichier externes
 #include "../Sources/menu.cpp"
@@ -13,6 +14,11 @@ int main() {
     array<int, NOMBRE_SCORE> scores{0, 0, 0};
     int indiceCourant = 0;
 
+    // génération aléatoire de la réponse
+    auto generateurAleatoire = std::random_device{};
+    auto generateur = mt19937{generateurAleatoire()};
+    auto distribution = uniform_int_distribution{BORNE_MIN, BORNE_MAX};
+
     while(continuer){
         //affichage du menu
         afficherMenu();
@@ -21,7 +27,7 @@ int main() {
         // traitement de la réponse du joueur au menu
         switch(choix){
             case ChoixMenu::JOUER:
-                scores[indiceCourant] = jouerPartie(3000);
+                scores[indiceCourant] = jouerPartie(distribution(generateur));
                 indiceCourant = (indiceCourant + 1) % NOMBRE_SCORE;
                 break;
 
